b25f62b247a556955a6de17fdfba13a80191bffa
michele.mongardi2 <michele.mongardi2@studio.unibo.it>
2023-03-24 00:05:55 +0100

    Task1 - 1b - Append Ok

--- src/main/scala/Tasks/PartOne.scala
+++ src/main/scala/Tasks/PartOne.scala
@@ -13,9 +13,20 @@ object PartOne extends App {
     case (Cons(_, t), 1) => t
     case (Cons(_, t), n) => drop(t, n-1)
 
+  println("***** Task1 - 1a - Drop *****")
+
   val lst = Cons(10, Cons(20, Cons(30, Nil())))
   println(drop(lst, 1)) // Cons (20 , Cons (30 , Nil ()))
   println(drop(lst, 2)) // Cons (30 , Nil ())
   println(drop(lst, 5)) // Nil ()
+
+  def append[A](left: List[A], right: List[A]): List[A] = (left, right) match
+    case (Nil(), _) => right
+    case (Cons(h, t), right) => Cons(h, append(t, right))
+
+  println("***** Task1 - 1b - Append *****")
+
+  val tail = Cons(40, Nil())
+  println(append(lst, tail)) // Cons (10 , Cons (20 , Cons (30 , Cons (40 , Nil ()))))
 
 }
